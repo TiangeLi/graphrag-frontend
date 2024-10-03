40f082cb0fd9a561a06c32d46d85d204cc3cd9e5
Tiange Li <5252737+TiangeLi@users.noreply.github.com>
2024-10-03 11:23:21 -0400

    build errors fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,10 +17,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <MyRuntimeProvider>
+    <MyRuntimeProvider serverUrl={process.env.NEXT_PUBLIC_BACKEND_URL_ALL_GUIDELINES || ''}>
       <html lang="en" className="h-full w-full">
         <body className={`${inter.className} h-full w-full`}>{children}</body>
       </html>
     </MyRuntimeProvider>
-  );
-}
+  )};
